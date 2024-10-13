5922103b382e7b433cf1f509efdff528f18edc0b
PedroLuizPrado <142911409+PedroLuizPrado@users.noreply.github.com>
2024-10-13 19:33:09 -0300

    cadastro

--- app/src/main/java/com/example/cp2herois/FirstFragment.kt
+++ app/src/main/java/com/example/cp2herois/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,36 +19,51 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FirstFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var nameInput: EditText
+    private lateinit var ageInput: EditText
+    private lateinit var emailInput: EditText
+    private lateinit var submitButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        // Inicializando os componentes
+        nameInput = view.findViewById(R.id.nome)
+        ageInput = view.findViewById(R.id.idade)
+        emailInput = view.findViewById(R.id.email)
+        submitButton = view.findViewById(R.id.submitButton)
+
+        // Configurando o OnClickListener do botão
+        submitButton.setOnClickListener {
+            val name = nameInput.text.toString()
+            val age = ageInput.text.toString()
+            val email = emailInput.text.toString()
+
+            // Criando um bundle com os dados
+            val bundle = Bundle()
+            bundle.putString("name", name)
+            bundle.putString("age", age)
+            bundle.putString("email", email)
+
+            // Criando e substituindo o fragmento
+            val secondFragment = SecondFragment()
+            secondFragment.arguments = bundle
+
+            fragmentManager?.beginTransaction()?.replace(R.id.fragment_container, secondFragment)?.commit()
+        }
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FirstFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+        // O resto do código permanece igual
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             FirstFragment().apply {
